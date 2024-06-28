6e1cd754d9c25ef332e39f4253d9d722cbe20f35
mmfay <mmfay3@gmail.com>
2024-06-28 04:06:36 -0700

    Moved methods to external

--- clientReq.php
+++ clientReq.php
@@ -1,87 +1,6 @@
 <?php 
     include 'external.php';
-    /*
-        Name: logout()
-        Description: Handles ending a user session.
-    */
-    function logout() {
-        session_start();
-        $_SESSION = array();
-        session_destroy();
-    }
-    /*
-        Name: insertRecord()
-        Description: Handles adding a new timecode to database.
-    */
-    function insertRecord($code, $desc) {
-        $query = "INSERT INTO TIMECODES (TIMECODE, DESCRIPTION, ISACTIVE, COMPANYCODE) VALUES ('" . $code . "','" . $desc . "',1,'" . $_SESSION["COMPCODE"] . "');";
-        executeSQL($query);
-        getTimeCodesTable();
-    }
-    /*
-        Name: insertUser()
-        Description: Handles adding a new user to a database.
-    */
-    function insertUser($user, $fname, $lname, $phone, $email, $password, $security) {
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $query = "INSERT INTO USERINFO (USERNAME, FIRSTNAME, LASTNAME, PHONE, EMAIL, PASSWORD, COMPANYCODE) VALUES ('" . $user . "','" . $fname . "','" . $lname . "','" . $phone . "','" . $email . "','" . $password . "','" . $_SESSION["COMPCODE"] . "');";
-        executeSQL($query);
-        $sqlInsert = "INSERT INTO USERSECURITY (USERNAME, SECURITYACCESS, COMPANYCODE) VALUES ('" . $user . "','" . $security . "','" . $_SESSION["COMPCODE"] . "');";
-        executeSQL($sqlInsert);
-        userList();
-    }
-    /*
-        Name: insertTimeCode()
-        Description: Handles adding a new timecode log to a database.
-    */
-    function insertTimeCode($timeCode, $type) {
-        updateOpenTimeCodes();
-        if (strcmp($type,'submit') == 0) {
-            $query = "INSERT INTO TIMECODESLOGGING (USERNAME, TIMECODE, TIMECODESTART, COMPANYCODE) VALUES ('" . $_SESSION['USERID'] . "','" . $timeCode . "', now(), '" . $_SESSION["COMPCODE"] . "');";
-            executeSQL($query);
-        }
-        dailyCodes();
-    }
-    /*
-        Name: updateOpenTimeCodes()
-        Description: Handles updating timecode log in a database.
-    */
-    function updateOpenTimeCodes() {
-        $query = "UPDATE TIMECODESLOGGING SET TIMECODEEND = NOW() WHERE USERNAME = '" . $_SESSION['USERID'] . "' AND TIMECODEEND is null AND COMPANYCODE = '" . $_SESSION["COMPCODE"] . "';";
-        executeSQL($query);
-    }
-    /*
-        Name: updateRecord()
-        Description: Handles updating timecode in a database.
-    */
-    function updateRecord($code, $active) {
-        $query = "UPDATE TIMECODES SET ISACTIVE = " . intval($active) . " WHERE TIMECODE = '" . $code . "' AND COMPANYCODE = '" . $_SESSION["COMPCODE"] . "';";
-        executeSQL($query);
-    }
-    /*
-        Name: deleteTimeCode()
-        Description: Handles deleting a timecode in a database.
-    */
-    function deleteTimeCode($timeCode) {
-        $query = "DELETE FROM TIMECODES WHERE TIMECODE = '" . $timeCode . "' AND COMPANYCODE = '" . $_SESSION["COMPCODE"] . "';";
-        executeSQL($query);
-        getTimeCodesTable();
-    }
-    /*
-        Name: executeSQL()
-        Description: Handles executing sql statements in a database.
-    */
-    function executeSQL($sqlQuery) {
-        $conn = getDatabaseConnection();
-        if ($conn->connect_error) {
-            die("connection failed: " . $conn->connect_error);
-        }
-        // prepare statement
-        $sql = $conn->prepare($sqlQuery);
-        // execute
-        $sql->execute();
-        $conn->close();
-    }
+
     // Retrieve the raw POST data
     $jsonData = file_get_contents('php://input');
     // Decode the JSON data into a PHP associative array
@@ -89,8 +8,6 @@
     // Check if decoding was successful
     if ($data !== null) {
     // Access the data and perform operations
-    //$name = $data['name'];
-    //$active = $data['isActive'];
     // Perform further processing or respond to the request
             $type = $data['title'];
             switch ($type) {
@@ -136,7 +53,7 @@
                             buildReportTable($timeRange, $userRange);
                             break;   
                         default:
-                            break;
+                            echo "error";
 
                     }
                 case "Logout":
@@ -147,9 +64,9 @@
             }
 
     } else {
-    // JSON decoding failed
-    http_response_code(400); // Bad Request
-    echo "Invalid JSON data";
+        // JSON decoding failed
+        http_response_code(400); // Bad Request
+        echo "Invalid JSON data";
     }
 
 ?>
